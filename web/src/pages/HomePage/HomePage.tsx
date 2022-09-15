359cc6b9f7c4f00d44e89c3557391c34779e60b4
jakubfiglak <jakub.figlak@netguru.pl>
2022-09-15 10:37:17 +0200

    feat: prepare login example

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,18 @@
 import { MetaTags } from '@redwoodjs/web'
+import { Toaster } from '@redwoodjs/web/dist/toast'
 
 import ArticlesCell from 'src/components/ArticlesCell'
+import LoginForm from 'src/components/LoginForm/LoginForm'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
+
+      <LoginForm />
       <ArticlesCell />
+
+      <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
     </>
   )
 }
